64ac09a56cb608d4866b0cfa407e674f2a0c9aa9
abdullah <abdullahakram2000@live.com>
2019-03-19 11:44:42 +0100

    changes homepage to home

--- wordcount/urls.py
+++ wordcount/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('',views.homepage,name='home'),
+    path('',views.home,name='home'),
     path('count/',views.count,name='count')
 ]
